2efef607f0e92f13db594781bd0bba7159b689af
rajala99 <rajala.harini99@gmail.com>
2022-12-28 08:42:51 +0530

    ADDED: code for equality of a line

--- src/com/bridgelabs/linecomparison/LineComparison.java
+++ src/com/bridgelabs/linecomparison/LineComparison.java
@@ -2,8 +2,23 @@ package com.bridgelabs.linecomparison;
 import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println(" Welcome to the Line comparison program ");
-        double x1, y1, x2, y2;
+        System.out.println("Welcome to Line Comparison Program");
+        Line line1 = getLine();
+        double lengthOfLine = getLength(line1);
+        System.out.println("length of line 1 is " + lengthOfLine);
+        System.out.println("enter value for second line");
+        Line line2 = getLine();
+        double lengthOfLine2 = getLength(line2);
+        System.out.println("length of line 1 is " + lengthOfLine2);
+        System.out.println(line1);
+        System.out.println(line2);
+        if(lengthOfLine == lengthOfLine2)
+            System.out.println("both lines are equal");
+        else
+            System.out.println("lines are not equal");
+    }
+    public static Line getLine() {
+        double x1,y1,x2,y2;
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter value for x1");
         x1 = scan.nextDouble();
@@ -13,7 +28,11 @@ public class LineComparison {
         x2 = scan.nextDouble();
         System.out.println("Enter value for y2");
         y2 = scan.nextDouble();
-        double lengthOfLine = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("length of line is " + lengthOfLine);
+        Line line = new Line(x1,y1,x2,y2);
+        return line;
+    }
+    public static double getLength(Line line){
+        double lengthOfLine = Math.sqrt(Math.pow(line.x2 - line.x1, 2) + Math.pow(line.y2 - line.y1, 2));
+        return lengthOfLine;
     }
 }
