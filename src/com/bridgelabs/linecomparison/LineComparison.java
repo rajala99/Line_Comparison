c1b32dd394d95026b637bf914f25e5c20e47ccf6
rajala99 <rajala.harini99@gmail.com>
2022-12-28 08:31:44 +0530

    ADDED: code for welcome message

---
+++ src/com/bridgelabs/linecomparison/LineComparison.java
@@ -0,0 +1,7 @@
+package com.bridgelabs.linecomparison;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to the Line comparison program ");
+    }
+}
